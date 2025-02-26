3bd43a1974e182d2dd7642e79daac4379747449c
ASHLEIGH_MARKgit config --global user.email ASHLEIGH.MARK27@GMAIL.COM <ashleigh.mark27@gmail.com>
2025-02-25 22:05:51 -0500

    Add bot code

--- markov.py
+++ markov.py
@@ -2,6 +2,8 @@
 
 import sys
 from random import choice
+import os
+import discord
 
 
 def open_and_read_file(filenames):
@@ -68,3 +70,25 @@ text = open_and_read_file(filenames)
 
 # Get a Markov chain
 chains = make_chains(text)
+
+
+# Bot code
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Successfully connected! Logged in as {client.user}.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.environ['DISCORD_TOKEN'])
+
